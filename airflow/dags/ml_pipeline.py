69887d0b1bb4d9f6b2886977722afc04d604340b
Mario Quiroga <marioquiroga2912@hotmail.com>
2025-05-07 13:42:36 +0000

    import_data working on airflow

--- airflow/dags/ml_pipeline.py
+++ airflow/dags/ml_pipeline.py
@@ -18,11 +18,12 @@ def create_docker_task(task_id, image, command):
         docker_url='unix://var/run/docker.sock',
         network_mode='bridge',
         mounts=[
-            Mount(source='/opt/airflow/dags', target='/opt/airflow/dags', type='bind'),
-            Mount(source='/opt/airflow/logs', target='/opt/airflow/logs', type='bind'),
-            Mount(source='/opt/airflow/plugins', target='/opt/airflow/plugins', type='bind'),
+            Mount(source='/home/ubuntu/MLOps_movie_recommandation/airflow/dags', target='/opt/airflow/dags', type='bind'),
+            Mount(source='/home/ubuntu/MLOps_movie_recommandation/airflow/logs', target='/opt/airflow/logs', type='bind'),
+            Mount(source='/home/ubuntu/MLOps_movie_recommandation/airflow/plugins', target='/opt/airflow/plugins', type='bind'),
             Mount(source='/var/run/docker.sock', target='/var/run/docker.sock', type='bind'),
         ],
+        force_pull=False,
     )
 
 # Définition du DAG
@@ -37,25 +38,25 @@ with DAG(
     # Création des tâches
     import_data = create_docker_task(
         task_id='import_data',
-        image='import_raw_data',
+        image='mlops_movie_recommandation_import_raw_data:latest',
         command='python import_raw_data.py',
     )
 
     build_features = create_docker_task(
         task_id='build_features',
-        image='build-features',
+        image='mlops_movie_recommandation_build-features:latest',
         command='python build_features.py',
     )
 
     train_model = create_docker_task(
         task_id='train_model',
-        image='train',
+        image='mlops_movie_recommandation_train:latest',
         command='python train.py',
     )
 
     evaluate_model = create_docker_task(
         task_id='evaluate_model',
-        image='evaluate',
+        image='mlops_movie_recommandation_evaluate:latest',
         command='python evaluate.py',
     )
 
