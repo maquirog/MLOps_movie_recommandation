2b3a30df8e62f4514af1193cd2c35ed2b1c43022
Ubuntu <ubuntu@ip-172-31-36-35.eu-west-1.compute.internal>
2025-05-16 11:08:59 +0000

    create experiment folder for the mlflow experiment service

---
+++ src/experiment/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "➡️  Lancement du serveur MLflow..."
+mlflow server \
+  --host 0.0.0.0 \
+  --port 8080 \
+  --backend-store-uri /app/mlruns \
+  --default-artifact-root /app/mlruns &
+
+# Attente pour laisser le temps au serveur de démarrer
+sleep 5
+
+echo "➡️  Lancement de l'expérience MLflow..."
+mlflow run src/experiment \
+  --env-manager=local \
+  --experiment-name=Movie_Recommandation_Model_Test \
+  #-P n_movies_metrics=20
+
+echo "✅ Expérience terminée. MLflow disponible sur http://localhost:8080"
+tail -f /dev/null
