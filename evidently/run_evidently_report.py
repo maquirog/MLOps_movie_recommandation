eb942b7d25e25a710ddbb8bfefe093cd380836cf
Ubuntu <ubuntu@ip-172-31-36-35.eu-west-1.compute.internal>
2025-06-03 20:57:58 +0000

    add report to evidently workspace

--- evidently/run_evidently_report.py
+++ evidently/run_evidently_report.py
@@ -2,9 +2,31 @@ import pandas as pd
 from evidently.report import Report
 from evidently.metric_preset import DataDriftPreset, DataQualityPreset
 from evidently.metrics import DatasetDriftMetric
+from evidently.ui.workspace import Workspace
 import os
 
 DRIFT_SHARE_THRESHOLD = 0.3  # Global variable for drift threshold
+
+def add_report_to_workspace(workspace, project_name, project_description, report):
+    """
+    Adds a report to an existing or new project in a workspace.
+    """
+    # Check if project already exists
+    project = None
+    for p in workspace.list_projects():
+        if p.name == project_name:
+            project = p
+            break
+
+    # Create a new project if it doesn't exist
+    if project is None:
+        project = workspace.create_project(project_name)
+        project.description = project_description
+
+    # Add report to the project
+    workspace.add_report(project.id, report)
+    print(f"New report added to project {project_name}")
+
 
 def main():
     # Load your datasets
@@ -27,6 +49,14 @@ def main():
     os.makedirs(os.path.dirname(output_path), exist_ok=True)  # <-- FIX
     report.save_html(output_path)
     print(f"Evidently drift & quality report saved to {output_path}")
+
+    # Save to Evidently workspace
+    workspace = Workspace.create(os.path.abspath("evidently/workspace"))
+    add_report_to_workspace(
+        workspace=workspace,
+        project_name="movies_drift_monitoring",
+        project_description="Evidently dashboard to monitor drift for movies data",
+        report=report)
 
 if __name__ == "__main__":
     main()
