1619d7a3e59167546b002e093291e8ed686c8aae
Ubuntu <ubuntu@ip-172-31-36-35.eu-west-1.compute.internal>
2025-06-07 18:36:34 +0000

    call API endpoints in experiment service

--- src/experiment/grid_search_experiment.py
+++ src/experiment/grid_search_experiment.py
@@ -1,16 +1,37 @@
 from mlflow import MlflowClient
 import mlflow
-import pandas as pd
-import numpy as np
-from sklearn.neighbors import NearestNeighbors
-from src.models.evaluate import load_user_favorites, evaluate_and_save_metrics
+import requests
 import os
+import json
 from itertools import product
 import argparse
 
-def main():
-    BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '../..'))
+API_URL = "http://api:8000"
+#API_URL = "http://localhost:8000"
 
+def call_train(hyperparams):
+    response = requests.post(f"{API_URL}/train", json={"hyperparameters": hyperparams})
+    response.raise_for_status()
+    print("✅ Model trained successfully.")
+    return response.json()
+
+def call_predict(user_ids, n_recommendations):
+    json_data = {
+        "user_ids": user_ids,
+        "n_recommendations": n_recommendations
+    }
+    response = requests.post(f"{API_URL}/predict", json=json_data)
+    response.raise_for_status()
+    print("✅ Predictions generated and saved.")
+    return response.json()
+
+def call_evaluate(k):
+    response = requests.post(f"{API_URL}/evaluate", json={"k": k})
+    response.raise_for_status()
+    print("✅ Evaluation complete.")
+    return response.json()
+
+def main():
     # Parse arguments
     parser = argparse.ArgumentParser()
     parser.add_argument('--n_movies_metrics', type=int, 
@@ -22,24 +43,10 @@ def main():
     mlflow.set_tracking_uri("http://127.0.0.1:5000")
     client = MlflowClient()
 
-    # Set experiment
-    artifact_path = "movie_reco"
-
     # Define parameter grid
     n_neighbors_list = [10, 20, 30]
     algorithm_list = ['ball_tree', 'kd_tree', 'brute']
     param_grid = list(product(n_neighbors_list, algorithm_list))
-
-    # Import Database
-    movie_matrix = pd.read_csv(os.path.join(BASE_DIR, 'data', 'processed', 'movie_matrix.csv'))
-    X = movie_matrix.drop("movieId", axis=1)
-
-    favorite_movies = load_user_favorites(os.path.join(BASE_DIR, 'data', 'processed', 'user_favorites.json'))
-    users = pd.read_csv(os.path.join(BASE_DIR, 'data', 'processed', 'user_matrix.csv'))
-    users_id = list(range(1, 51))
-    filtered_users = users[users["userId"].isin(users_id)]
-    original_ids = filtered_users["userId"].values
-    filtered_users = filtered_users.drop("userId", axis=1)
 
     # For tracking best run
     best_coverage = -1
@@ -49,35 +56,26 @@ def main():
     for n_neighbors, algorithm in param_grid:
         print(f"\nTraining with n_neighbors={n_neighbors}, algorithm={algorithm}")
 
-        params = {
+        hyperparams = {
             'n_neighbors': n_neighbors,
             'algorithm': algorithm
         }
-        # Train model
-        model = NearestNeighbors(**params).fit(X)
 
-        # Get recommendations
-        _, indices = model.kneighbors(filtered_users)
-        selection = indices[:, :10]
-        recommended_movies = {
-            int(user_id): list(map(int, movie_indices))
-            for user_id, movie_indices in zip(original_ids, selection)
-        }
+        call_train(hyperparams)
 
-        # Evaluate model (metrics calculated on 10 reco from 50 users)
-        metrics = evaluate_and_save_metrics(
-            favorites=favorite_movies,
-            recommendations=recommended_movies,
-            movies_csv=os.path.join(BASE_DIR, 'data', 'processed', 'movie_matrix.csv'),
-            k=args.n_movies_metrics)
+        call_predict(user_ids=list(range(1, 4)), n_recommendations=10)
+
+        metrics = call_evaluate(k=args.n_movies_metrics)
+
+        BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '../..'))
+        metrics_path = os.path.join(BASE_DIR, "metrics", "scores.json")
+        with open(metrics_path, "r") as f:
+            metrics = json.load(f)
 
         # Store information in tracking server
         with mlflow.start_run() as run:
-            mlflow.log_params(params)
+            mlflow.log_params(hyperparams)
             mlflow.log_metrics(metrics)
-            mlflow.sklearn.log_model(
-                sk_model=model, artifact_path=artifact_path
-            )
             run_id = run.info.run_id
             print(f"Logged run_id: {run_id} with metrics: {metrics}")
 
@@ -85,7 +83,7 @@ def main():
             if metrics.get(f'coverage_{str(args.n_movies_metrics)}', 0) > best_coverage:
                 best_coverage = metrics[f'coverage_{str(args.n_movies_metrics)}']
                 best_run_id = run_id
-                best_params = params
+                best_params = hyperparams
 
     print("\n=== Best run based on 'coverage' ===")
     print(f"Run ID: {best_run_id}")
