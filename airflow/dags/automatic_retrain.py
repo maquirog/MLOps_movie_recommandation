7e416dbee93e2befc43d45858901eccd821dd8ca
Ubuntu <ubuntu@ip-172-31-36-35.eu-west-1.compute.internal>
2025-06-30 10:45:39 +0000

    update retrain workflow with DockerOperator

--- airflow/dags/automatic_retrain.py
+++ airflow/dags/automatic_retrain.py
@@ -2,24 +2,23 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.providers.docker.operators.docker import DockerOperator
 from docker.types import Mount
+#from src.models.check_and_update_model import check_and_update_model
 from datetime import datetime
 import requests
 import json
 import os
 import subprocess
 import yaml
+from pathlib import Path
 
-API_URL = "http://localhost:8000"
-
-#default_args = {
-#    'start_date': datetime(2024, 1, 1),
-#    'catchup': False,
-#}
+default_args = {
+    'owner': 'airflow',
+    'retries': 1,
+}
 
 HOST_PATH = os.getenv("HOST_PATH")
-#HOST_PATH = "/home/ubuntu/MLOps_movie_recommandation"
 
-vol1 = os.path.join(HOST_PATH, 'data')
+vol1 = os.path.join(HOST_PATH, 'mlruns')
 vol2 = os.path.join(HOST_PATH, 'models')
 vol3 = os.path.join(HOST_PATH, 'metrics')
 vol4 = os.path.join(HOST_PATH, 'airflow/dags')
@@ -35,8 +34,13 @@ def create_docker_task(task_id, image, command):
         auto_remove=True,
         docker_url='unix://var/run/docker.sock',
         network_mode='bridge',
+        working_dir='/app',
+        environment={
+            "PYTHONPATH": "/app",
+            "HOST_PROJECT_PATH": HOST_PATH,
+        },
         mounts=[
-            Mount(source=vol1, target='/app/data', type='bind'),
+            Mount(source=vol1, target='/app/mlruns', type='bind'),
             Mount(source=vol2, target='/app/models', type='bind'),
             Mount(source=vol3, target='/app/metrics', type='bind'),
             Mount(source=vol4, target='/opt/airflow/dags', type='bind'),
@@ -53,43 +57,55 @@ def check_and_update_model():
         current_metrics = json.load(f)
 
     # Cherche le meilleur run dans mlruns
-    mlruns_dir = "mlruns"
+    mlruns_dir = Path("mlruns").resolve()
     best_coverage = current_metrics.get("coverage_10", 0)
     best_model_path = None
+    best_metrics_path = None
 
     for root, dirs, files in os.walk(mlruns_dir):
         for file in files:
-            if file == "metrics.json":
-                with open(os.path.join(root, file)) as mf:
-                    try:
+            if file == "scores.json" and "artifacts/metrics" in str(root):
+                metrics_path = Path(root) / file
+                try:
+                    with open(metrics_path) as mf:
                         m = json.load(mf)
                         if m.get("coverage_10", 0) > best_coverage:
-                            best_coverage = m["coverage_10"]
-                            best_model_path = os.path.join(root, "..", "artifacts", "model", "model.pkl")
-                    except Exception as e:
-                        continue
+                            run_dir = Path(root).parent.parent 
+                            model_candidate = run_dir / "artifacts" / "model" / "model.pkl"
+                            if model_candidate.exists():
+                                best_coverage = m["coverage_10"]
+                                best_model_path = model_candidate
+                                best_metrics_path = metrics_path
 
-    if best_model_path:
-        print(f"New model is better. Updating prod model. coverage_10: {best_coverage}")
-        subprocess.run(["cp", best_model_path, "models/model.pkl"], check=True)
-        subprocess.run(["dvc", "add", "models/model.pkl"], check=True)
-        subprocess.run(["git", "commit", "-am", "Auto-update best model"], check=True)
+                except Exception as e:
+                    continue
+
+    if best_model_path and best_metrics_path:
+        print(f"✅ New model is better. Updating prod model. coverage_10: {best_coverage}")
+
+        # Copier new prod model + new prod model metrics
+        subprocess.run(["cp", best_model_path, "models/model_prod.pkl"], check=True)
+        subprocess.run(["cp", best_metrics_path, "metrics/scores_model_prod.json"], check=True)
+
+        # DVC commit prod model
+        subprocess.run(["dvc", "commit", "models/model_prod.pkl"], check=True)
+
+        # Git add + commit new prod model metrics
+        subprocess.run(["git", "add", "metrics/scores_model_prod.json"], check=True)
+        subprocess.run(["git", "commit", "-m", "Auto-update model_prod and scores_model_prod"], check=True)
+
+        # Git & DVC push
         subprocess.run(["dvc", "push"], check=True)
-
-        new_metrics_path = os.path.join(os.path.dirname(best_model_path), "..", "metrics.json")
-        subprocess.run(["cp", new_metrics_path, "metrics/scores.json"], check=True)
-        subprocess.run(["git", "add", "metrics/scores.json"], check=True)
-        subprocess.run(["git", "commit", "-m", "Update metrics from best run"], check=True)
         subprocess.run(["git", "push"], check=True)
     else:
-        print("No better model found. Keeping current production model.")
+        print("⚠️ No better model found. Keeping current production model.")
 
 
 with DAG(
     dag_id="automatic_retrain",
-    #default_args=default_args,
+    default_args=default_args,
     description="Auto retrain if new model is better",
-    schedule_interval="@weekly",  # Exécution manuelle
+    schedule_interval="@weekly",
     start_date=datetime(2024, 1, 1),
     catchup=False,
     tags=['ml', 'retrain'],
@@ -98,12 +114,18 @@ with DAG(
     experiment_task = create_docker_task(
         task_id='run_grid_search_task',
         image='experiment:latest',
-        command='python src/experiment/entrypoint.sh',
+        command=["bash", "src/experiment/entrypoint.sh"],
     )
 
-    check_update_task = PythonOperator(
-        task_id="run_check_update_task",
-        python_callable=check_and_update_model,
+    #check_update_task = PythonOperator(
+    #    task_id="run_check_update_prod_model_task",
+    #    python_callable=check_and_update_model,
+    #)
+
+    check_update_task = create_docker_task(
+        task_id='run_check_update_prod_model_task',
+        image='check_update_prod_model:latest',
+        command=["python", "src/models/check_update_prod_model.py"],
     )
 
     experiment_task >> check_update_task
--- src/models/check_update_prod_model.py
+++ src/models/check_update_prod_model.py
@@ -7,11 +7,10 @@ import shutil
 
 def check_and_update_model():
     print("Comparing new model to current production...")
-    with open("metrics/scores_test.json") as f:
+    with open("metrics/scores_model_prod.json") as f:
         current_metrics = json.load(f)
 
     # Cherche le meilleur run dans mlruns
-    #mlruns_dir = "mlruns"
     mlruns_dir = Path("mlruns").resolve()
     best_coverage = current_metrics.get("coverage_10", 0)
     best_model_path = None
@@ -25,39 +24,31 @@ def check_and_update_model():
                     with open(metrics_path) as mf:
                         m = json.load(mf)
                         if m.get("coverage_10", 0) > best_coverage:
-                            # Revenir au dossier run
                             run_dir = Path(root).parent.parent 
                             model_candidate = run_dir / "artifacts" / "model" / "model.pkl"
                             if model_candidate.exists():
                                 best_coverage = m["coverage_10"]
                                 best_model_path = model_candidate
-                                print(best_model_path)
                                 best_metrics_path = metrics_path
-                                print(best_coverage)
+
                 except Exception as e:
                     continue
 
     if best_model_path and best_metrics_path:
         print(f"✅ New model is better. Updating prod model. coverage_10: {best_coverage}")
 
-        # Copier modèle + métriques
+        # Copier new prod model + new prod model metrics
         subprocess.run(["cp", best_model_path, "models/model_prod.pkl"], check=True)
         subprocess.run(["cp", best_metrics_path, "metrics/scores_model_prod.json"], check=True)
-        #shutil.copy(best_metrics_path, "metrics/scores_model_prod.json")
 
-        # Commit DVC
+        # DVC commit prod model
         subprocess.run(["dvc", "commit", "models/model_prod.pkl"], check=True)
 
-        # Git add + commit + push
+        # Git add + commit new prod model metrics
         subprocess.run(["git", "add", "metrics/scores_model_prod.json"], check=True)
         subprocess.run(["git", "commit", "-m", "Auto-update model_prod and scores_model_prod"], check=True)
-        #try:
-        #    subprocess.run(["git", "commit", "-m", "Auto-update metrics"], check=True, capture_output=True, text=True)
-        #except subprocess.CalledProcessError as e:
-        #    if "nothing to commit" in e.stderr.lower():
-        #        print("Aucun changement à committer, tout est à jour.")
-        #else:
-        #    raise
+
+        # Git & DVC push
         subprocess.run(["dvc", "push"], check=True)
         subprocess.run(["git", "push"], check=True)
     else:
