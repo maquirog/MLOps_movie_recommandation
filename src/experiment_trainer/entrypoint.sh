96e8202fdd257b12225d10401b644db4b4944154
Rubenchek <rubenchek@gmail.com>
2025-07-04 14:10:46 +0200

    Fix bugs (call API & loop)

--- src/experiment_trainer/entrypoint.sh
+++ src/experiment_trainer/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Arguments : $1 = experiment_name (optionnel), $2 = hyperparams JSON string (optionnel)
+
 # Attente pour laisser le temps au serveur de démarrer
 echo "➡️  Attente que MLflow server soit prêt..."
 for i in {1..20}; do
@@ -12,17 +14,30 @@ for i in {1..20}; do
   fi
 done
 
-# Génère le nom d'expérimentation dynamiquement
-now=$(date +"%Y-%m-%d_%H-%M-%S")
-experiment_name="weekly_experiment_${now}"
+# Gestion du nom de l'expérience
+if [ -z "$1" ]; then
+  now=$(date +"%Y-%m-%d_%H-%M-%S")
+  experiment_name="weekly_experiment_${now}"
+else
+  experiment_name="$1"
+fi
+echo "ℹ️  Nom de l'expérience : $experiment_name"
 
 # Crée l'expérience weekly
 mlflow experiments create --experiment-name "$experiment_name" || true
 
+
+# Prépare l’argument hyperparams s’il est fourni
+if [ -z "$2" ]; then
+  hyperparams_arg=""
+  echo "➡️  Lancement de l'expérience MLflow avec hyperparams prédifinis localement"
+else
+  hyperparams_arg="--hyperparams_dict '$2'"
+  echo "➡️  Lancement de l'expérience MLflow avec hyperparams : $hyperparams_arg"
+fi
+
 echo "➡️  Lancement de l'expérience MLflow..."
-#python src/experiment_trainer/grid_search_experiment.py
-mlflow run src/experiment_trainer \
-  --env-manager=local \
-  -P experiment_name="$experiment_name"
+python  -m src.experiment_trainer.grid_search_experiment --experiment_name "$experiment_name" $hyperparams_arg
+
 
 echo "✅ Expérience terminée. MLflow disponible sur http://localhost:5050"
