616203c2183ece5f1b3086d31dc4a42be42f55e6
Rubenchek <rubenchek@gmail.com>
2025-07-10 11:11:19 +0200

    clean experiments before airflow tests

--- src/global.sh
+++ src/global.sh
@@ -1,4 +1,9 @@
 # mettre à jour .env.local
 # créer réseau ml-net
+docker network create mlops-net
+# importer les data et créer les feature
+docker-compose up import_raw_data
+docker-compose up build_features
 # lancer api
+docker-compose up api
 # lancer airflow
